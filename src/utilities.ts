d38d2f7519349a0147900a8067e897b8e9006df2
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-01-31 10:56:22 +0000

    List component
    Moved the list to a component called PostList because it is used twice on the site.

--- src/utilities.ts
+++ src/utilities.ts
@@ -5,6 +5,17 @@ const formatDate = (date: string): string => {
     day: 'numeric'
   })};
 
+  const capitalizeSentence = (input: string): string => {
+    const words = input.split(' ');
+
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substring(1);
+    }
+
+    return words.join(' ');
+  }
+
   export {
-    formatDate
+    formatDate,
+    capitalizeSentence
   }
