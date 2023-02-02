79fcdbf6a02e9656aa7e0f840be806ca67a042eb
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-02-02 11:11:27 +0000

    Netlify Deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,18 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
-
 import sitemap from "@astrojs/sitemap";
-
 import compress from "astro-compress";
+import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://sean.sew.io",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-    compress({
-      img: false
-    })
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), compress({
+    img: false
+  })],
+  output: "server",
+  adapter: netlify()
 });
