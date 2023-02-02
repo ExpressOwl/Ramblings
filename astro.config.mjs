a1efb7981b9cd7ad3fbcbfbd785a83a72808b7c4
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-02-02 11:24:18 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,13 +6,20 @@ import compress from "astro-compress";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://sean.sew.io",
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), compress({
-    img: false
-  })],
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap(),
+    compress({
+      img: false,
+    }),
+  ],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
